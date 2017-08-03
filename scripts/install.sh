a6b547d7718caf7fe1ba81733f80e036ca723b8c
Alexey Velikiy <gmpota@gmail.com>
2017-08-03 15:38:46 +0300

    grafana types install

---
+++ scripts/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ -d "types-grafana" ]; then
+  rm -rf types-grafana
+fi
+
+git clone https://github.com/corpglory/types-grafana
+pwd
+cp -rf types-grafana/app node_modules/@types/
+rm -rf types-grafana
