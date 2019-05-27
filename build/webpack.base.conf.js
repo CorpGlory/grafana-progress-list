d1094c4ffe032bb638cab9c172fc7723899b7927
Alexey Velikiy <av@corpglory.com>
2019-05-27 04:55:13 +0300

    update build system

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+function resolve(dir) {
+  return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+  target: 'node',
+  context: resolve('src'),
+  entry: {
+    './module': './module.ts'
+  },
+  output: {
+    filename: '[name].js',
+    path: resolve('dist'),
+    libraryTarget: 'amd'
+  },
+  externals: [
+    // remove the line below if you don't want to use buildin versions
+    'lodash', 'moment', 'angular',
+    function(context, request, callback) {
+      var prefix = 'grafana/';
+      if (request.indexOf(prefix) === 0) {
+        return callback(null, request.substr(prefix.length));
+      }
+      callback();
+    }
+  ],
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new CopyWebpackPlugin([
+      { from: 'plugin.json' },
+      { from: 'img/*' },
+      { from: 'panel/graph_panel/plugin.json', to: 'panel/graph_panel/plugin.json' },
+      { from: 'panel/graph_panel/partials/*' },
+      { from: 'datasource/plugin.json', to: 'datasource/plugin.json' },
+    ])
+  ],
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/, 
+        loaders: [
+          'ts-loader'
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.html$/,
+        use: 'raw-loader'
+      },
+      {
+        test: /jquery\.flot\.(?!events)/,
+        loaders: [
+          'imports-loader?jQuery=jquery'
+        ]
+      },
+      {
+        test: /jquery\.flot\.events/,
+        loaders: [
+          'imports-loader?jQuery=jquery,lodash=lodash,angular=angular,tetherDrop=tether-drop'
+        ]
+      }
+    ]
+  }
+}
